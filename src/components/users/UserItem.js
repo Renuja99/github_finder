cfe97abb102e57f86f44e800736610b3f36d838c
Renuja De Costa <devmindadeco@gmail.com>
2020-10-21 14:03:09 +0530

    Added Navbar and UserItems component

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+   
+    constructor(){
+        super();
+        console.log(123);
+    }    
+   
+   
+    render() {
+        return (
+        
+        <div>  User Item </div>
+        )
+    }
+}
+
+export default UserItem
