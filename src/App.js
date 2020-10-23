c56bd1554b8aa5aa32a9e5a0bd06fa092da9940b
Renuja De Costa <devmindadeco@gmail.com>
2020-10-23 16:24:21 +0530

    Requests and Updating State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  state ={
+
+    users: [],
+    loading : false,
+
+  }
+
+  async componentDidMount(){
+
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({users : res.data , loading: false});
+  }
+
+
   render() {
     return (
       <div className='App'>
@@ -11,7 +27,7 @@ class App extends Component {
 
         <div className="container">
         
-        <Users />
+        <Users loading ={this.state.loading} users={this.state.users} />
 
         </div>
         
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types'
 
 
 const Navbar =({icon, title})=> {
-
-  
-
-
     
         return (
             <nav className="navbar bg-primary">
